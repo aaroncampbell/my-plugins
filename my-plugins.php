5f18d7c123fc1f11e1f057517927f9407caaff8e
Aaron D. Campbell <Aaron@AaronDCampbell.com>
2020-07-04 19:35:43 -0500

    Complete change to using REST API actions and such to wortk with block editor

--- my-plugins.php
+++ my-plugins.php
@@ -15,30 +15,23 @@ class myPlugins {
 	 */
 	static $instance = false;
 
-	private $_additional_plugin_info;
-
 	/**
 	 * Private for force the singleton
 	 */
 	private function __construct() {
-		$this->_additional_plugin_info = apply_filters( 'my-plugins-additional-plugin-info', array(
-			'github_url' => 'Github URL',
-		) );
-
+		add_action( 'admin_init',                           array( $this, 'action_admin_init'                    ) );
+		add_action( 'update_plugin_info',                   array( $this, 'update_plugin_info'                   ) );
 		add_action( 'after_setup_theme',                    array( $this, 'after_setup_theme'                    ) );
-		add_action( 'admin_menu',                           array( $this, 'admin_menu'                           ) );
-		add_action( 'wp_ajax_sort',                         array( $this, 'wp_ajax_sort'                         ) );
-		add_action( 'updated_post_meta',                    array( $this, 'store_plugin_last_updated' ), null, 4   );
-		add_action( 'add_post_metadata',                    array( $this, 'store_plugin_last_updated' ), null, 4   );
-		add_action( 'parse_query',                          array( $this, 'order_plugins'                        ) );
-		add_filter( 'plugin_info',                          array( $this, 'plugin_info'                          ) );
-		add_action( 'save_post',                            array( $this, 'save_post'                            ) );
-		add_action( 'save_post',                            array( $this, 'save_meta'                            ) );
-		add_action( 'add_meta_boxes_plugin',                array( $this, 'add_meta_boxes_plugin'                ) );
+		add_action( 'rest_after_insert_plugin',             array( $this, 'rest_after_insert_plugin'             ) );
 		add_action( 'the_content',                          array( $this, 'the_content'                          ) );
 		add_action( 'wp_footer',                            array( $this, 'wp_footer'                            ) );
 		add_action( 'init',                                 array( $this, 'init'                                 ) );
 		add_action( 'enqueue_block_editor_assets',          array( $this, 'sidebar_script_enqueue'               ) );
+
+		// For sorting
+		add_action( 'admin_menu',                           array( $this, 'admin_menu'                           ) );
+		add_action( 'wp_ajax_sort',                         array( $this, 'wp_ajax_sort'                         ) );
+		add_action( 'parse_query',                          array( $this, 'order_plugins'                        ) );
 	}
 
 	/**
@@ -119,8 +112,6 @@ class myPlugins {
 			)
 		);
 
-		// @TODO register meta to store plugin info
-
 		// Register script for editor sidebar
 		$asset_file = include( plugin_dir_path( __FILE__ ) . 'build/index.asset.php');
 
@@ -148,26 +139,10 @@ class myPlugins {
 	}
 
 	public function admin_menu() {
-		add_meta_box(
-			'plugininfo',
-			__( 'Plugin Info', 'plugin_info' ),
-			array( $this, 'meta_box' ),
-			'plugin',
-			'side'
-		);
-
-
 		$my_plugins_sort_plugins = add_submenu_page( 'edit.php?post_type=plugin', __( 'Sort Plugins', 'my-plugins' ), __( 'Sort Plugins', 'my-plugins' ), 'edit_posts', 'sort-plugins', array( $this, 'sort' ) );
 
 		add_action( 'admin_print_styles-' . $my_plugins_sort_plugins,  array( $this, 'print_sort_styles'  ) );
 		add_action( 'admin_print_scripts-' . $my_plugins_sort_plugins, array( $this, 'print_sort_scripts' ) );
-	}
-
-	public function meta_box( $post ) {
-		?>
-		<label for="plugin_info"><?php _e( 'Plugin slug:', 'my-plugins' ); ?></label>
-		<input type="text" name="plugin_info" id="plugin_info" value="<?php esc_attr_e( get_post_meta( $post->ID, 'plugin', true ) ); ?>" />
-		<?php
 	}
 
 	public function print_sort_styles() {
@@ -227,16 +202,9 @@ class myPlugins {
 		die(1);
 	}
 
-	public function store_plugin_last_updated( $meta_id, $object_id, $meta_key, $meta_value ) {
-		if ( 'plugin-info' == $meta_key )
-			update_post_meta( $object_id, 'plugin-last-updated', $meta_value['updated_raw'] );
-	}
-
 	public function order_plugins ( $query ) {
 		if ( ! is_admin() && $query->is_main_query() && is_post_type_archive( array( 'plugin' ) ) ) {
 			$query->set( 'nopaging', true );
-			//$query->set( 'meta_key', 'plugin-last-updated' );
-			//$query->set( 'orderby', 'meta_value' );
 			$query->set( 'orderby', 'menu_order' );
 			$query->set( 'order', 'ASC' );
 		}
@@ -261,70 +229,96 @@ class myPlugins {
 		return "[code lang='{$lang}']" . html_entity_decode($matches[2], ENT_QUOTES) . '[/code]';
 	}
 
-	public function plugin_info( $info ) {
-		$info['star_rating'] = $this->_get_stars( $info['rating_raw'] );
-		return $info;
+	/**
+	 * @param WP_Post         $post     Inserted or updated post object.
+	 * @param WP_REST_Request $request  Request object.
+	 * @param bool            $creating True when creating a post, false when updating.
+	 */
+	public function rest_after_insert_plugin( $post ) {
+		$plugin_info = $this->get_plugin_info( get_registered_metadata( 'post', $post->ID, '_plugin_slug' ) );
+		if ( ! is_wp_error( $plugin_info ) ) {
+			update_post_meta( $post->ID, '_plugin_info', $plugin_info );
+			if ( $plugin_info->banners && is_array( $plugin_info->banners ) ) {
+				$banner = empty( $plugin_info->banners['high'] )? $plugin_info->banners['low'] : $plugin_info->banners['high'];
+			}
+			$this->_sideload_image_to_featured_image( $banner, $post->ID );
+		}
 	}
 
-	public function save_post( $post_ID ) {
+	public function get_plugin_info( $slug = null ) {
+		if ( empty( $slug ) )
+			return false;
 
-		if ( wp_is_post_revision( $post_ID ) or wp_is_post_autosave( $post_ID ) )
+		admin_url();
+
+		require_once ABSPATH . 'wp-admin/includes/plugin-install.php';
+
+		$slug = sanitize_title( $slug );
+		return plugins_api( 'plugin_information', array( 'slug' => $slug ) );
+	}
+
+	public function action_admin_init() {
+		if ( ! wp_next_scheduled( 'update_plugin_info' ) ) {
+			// @TODO should this be hourly?
+			wp_schedule_event( time(), 'hourly', 'update_plugin_info' );
+		}
+	}
+
+	public function update_plugin_info() {
+		$q = new WP_Query;
+
+		$posts = $q->query( array(
+			'posts_per_page' => -1,
+			'meta_key'       => '_plugin_slug',
+			'post_type'      => 'plugin'
+		) );
+
+		if ( ! count( $posts ) )
 			return;
 
-		if ( empty( $_POST['plugin_info'] ) )
-			return;
-
-		$plugin = trim( stripslashes( $_POST['plugin_info'] ) );
-		$old_plugin = get_post_meta( $post_ID, 'plugin' );
-
-		// If the plugin has changed or there's no thumbnail
-		if ( ! get_post_meta( $post_ID, '_thumbnail_id' ) || $plugin != $old_plugin ) {
-			// Pull in the header asset as the featured image, trying 2x and falling back to standard
-			$url = 'http://ps.w.org/%s/assets/banner-%s.png';
-			if ( ! $this->_sideload_image_to_featured_image( sprintf( $url, $info['slug'], '1544×500' ), get_the_ID() ) ) {
-				$this->_sideload_image_to_featured_image( sprintf( $url, $info['slug'], '772x250' ), get_the_ID() );
+		foreach ( $posts as $post ) {
+			$plugin_info = $this->get_plugin_info( get_registered_metadata( 'post', $post->ID, '_plugin_slug' ) );
+			if ( ! is_wp_error( $plugin_info ) ) {
+				update_post_meta( $post->ID, '_plugin_info', $plugin_info );
 			}
 		}
 	}
 
 	private function _sideload_image_to_featured_image( $file, $post_id, $desc = null ) {
-		if ( ! empty( $file ) ) {
-			// Set variables for storage
-			// fix file filename for query strings
-			preg_match( '/[^\?]+\.(jpe?g|jpe|gif|png)\b/i', $file, $matches );
-			$file_array['name'] = basename( $matches[0] );
-			// Download file to temp location
-			$file_array['tmp_name'] = download_url( $file );
+		// Has download_url() which is used by media_sideload_image()
+		require_once ABSPATH . 'wp-admin/includes/file.php';
+		// Has wp_read_image_metadata() which is used by media_sideload_image()
+		require_once ABSPATH . 'wp-admin/includes/image.php';
+		// Has media_sideload_image()
+		require_once ABSPATH . 'wp-admin/includes/media.php';
+		$image = media_sideload_image( $file, $post_id, $desc, 'id' );
 
-			// If error storing temporarily, return the error.
-			if ( is_wp_error( $file_array['tmp_name'] ) ) {
-				return $file_array['tmp_name'];
-			}
-
-			// do the validation and storage stuff
-			$id = media_handle_sideload( $file_array, $post_id, $desc );
-			// If error storing permanently, unlink
-			if ( is_wp_error( $id ) ) {
-				@unlink( $file_array['tmp_name'] );
-				return $id;
-			}
-			update_post_meta( $post_id, '_thumbnail_id', $id );
-			return $id;
+		if ( is_wp_error( $image ) ) {
+			return;
 		}
-		return false;
+
+		return set_post_thumbnail( $post_id, $image );
 	}
 
 	private function _get_stars( $rating ) {
+		$num_stars = round( 5 * $rating / 100, 1 );
+		$stars_title = sprintf( _n( '%s star', '%s stars', $num_stars, 'my-plugins' ), $num_stars );
 		return <<<STARS
 		<div class="star-holder">
-			<svg xmlns="http://www.w3.org/2000/svg" width="1275" height="240" viewBox="0 0 255 48">
-				<title>Five Stars</title>
+			<svg
+				xmlns:svg="http://www.w3.org/2000/svg"
+				xmlns="http://www.w3.org/2000/svg"
+				version="1.1"
+				height="240"
+				width="1275"
+				viewBox="0 0 255 48">
+				<title>{$stars_title}</title>
 				<defs>
-					<style type="text/css"><![CDATA[
-						.fill {
-							width: {$rating}%;
-						}
-					]]></style>
+					<style>
+					.fill {
+						width: {$rating}%;
+					}
+					</style>
 					<mask id="star-mask">
 						<path fill="white" d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"/>
 						<path fill="white" d="m76,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"/>
@@ -332,7 +326,6 @@ class myPlugins {
 						<path fill="white" d="m178,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"/>
 						<path fill="white" d="m229,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"/>
 					</mask>
-
 				</defs>
 				<!-- This is the fill. It needs to be behind the content and the top -->
 				<rect x="0" y="0" width="0" height="48" rx="1" class="fill" mask="url(#star-mask)"></rect>
@@ -347,101 +340,62 @@ class myPlugins {
 STARS;
 	}
 
-	public function add_meta_boxes_plugin( $post_type ) {
-		add_meta_box( 'myplugins-plugin-info', __( 'Additional Plugin Info', 'my-plugins' ), array( $this, 'metabox' ), 'plugin', 'normal', 'high' );
-	}
-
-	public function metabox( $post ) {
-		foreach ( $this->_additional_plugin_info as $key => $label ) {
-			$value = get_post_meta( $post->ID, $key, true );
-			?>
-            <div class="field field-text">
-                <label for="<?php echo esc_attr( $key ) ?>"><?php echo esc_html( $label ) ?></label>
-                <input class="widefat" type="text" name="<?php echo esc_attr( $key ); ?>" value="<?php echo esc_attr( $value ) ?>" id="<?php echo esc_attr( $key ) ?>" />
-            </div>
-			<?php
-		}
-	}
-
-	public function save_meta( $id ) {
-		foreach ( $this->_additional_plugin_info as $key => $label ) {
-			if ( isset( $_POST[$key] ) ) {
-				$_POST[$key] = stripslashes( $_POST[$key] );
-				update_post_meta( $id, $key, $_POST[$key] );
-			} else {
-				delete_post_meta( $id, $key );
-			}
-		}
-	}
-
 	public function the_content( $content ) {
 		if ( ! is_singular( 'plugin' ) ) {
 			return $content;
 		}
-
-		$plugin_info = array();
-		$plugin_info['slug'] = get_post_meta( get_the_ID(), 'plugin', true );
-		$plugin_info['faq'] = get_plugin_info( $plugin_info['slug'], 'faq' );
-		$plugin_info['changelog'] = get_plugin_info( $plugin_info['slug'], 'changelog' );
-		$plugin_info['installation'] = get_plugin_info( $plugin_info['slug'], 'installation' );
-		$plugin_info['description'] = get_plugin_info( $plugin_info['slug'], 'description' );
-
-		foreach ( $plugin_info as &$p_info ) {
-			$p_info = do_shortcode( myPlugins::get_instance()->highlight_syntax( $p_info ) );
+		$plugin_info = get_post_meta( get_the_ID(), '_plugin_info', true );
+		if ( ! $plugin_info ) {
+			$plugin_info = $this->get_plugin_info( get_registered_metadata( 'post', get_the_ID(), '_plugin_slug' ) );
+			if ( ! $plugin_info || is_wp_error( $plugin_info ) ) {
+				return $content;
+			}
 		}
-		$installation_tab = $installation_tab_content = $faq_tab = $faq_tab_content = $changelog_tab = $changelog_tab_content = '';
-		if ( !empty( $plugin_info['installation'] ) ) {
-			$installation_tab = '<li><a href="#tabs-installation">Installation</a></li>';
-			$installation_tab_content = '<div id="tabs-installation" class="ui-tabs-hide">' . $plugin_info['installation'] . '</div>';
-		}
-		if ( !empty( $plugin_info['faq'] ) ) {
-			$faq_tab = '<li><a href="#tabs-faq">FAQ</a></li>';
-			$faq_tab_content = '<div id="tabs-faq" class="ui-tabs-hide">' . $plugin_info['faq'] . '</div>';
-		}
-		if ( !empty( $plugin_info['changelog'] ) ) {
-			$changelog_tab = '<li><a href="#tabs-changelog">Changelog</a></li>';
-			$changelog_tab_content = '<div id="tabs-changelog" class="ui-tabs-hide">' . $plugin_info['changelog'] . '</div>';
-		}
-
 		if ( ! empty( $content ) ) {
-			$plugin_info['description'] = $content;
+			$plugin_info->sections['description'] = $content;
+		}
+		$installation_tab = $installation_tab_content = $faq_tab = $faq_tab_content = $changelog_tab = $changelog_tab_content = $details_tab = $details_tab_content = '';
+
+		if ( ! empty( $plugin_info->sections['installation'] ) ) {
+			$installation_tab = '<li><a href="#tabs-installation">Installation</a></li>';
+			$installation_tab_content = '<div id="tabs-installation" class="ui-tabs-hide">' . $plugin_info->sections['installation'] . '</div>';
+		}
+		if ( !empty( $plugin_info->sections['faq'] ) ) {
+			$faq_tab = '<li><a href="#tabs-faq">FAQ</a></li>';
+			$faq_tab_content = '<div id="tabs-faq" class="ui-tabs-hide">' . $plugin_info->sections['faq'] . '</div>';
+		}
+		if ( !empty( $plugin_info->sections['changelog'] ) ) {
+			$changelog_tab = '<li><a href="#tabs-changelog">Changelog</a></li>';
+			$changelog_tab_content = '<div id="tabs-changelog" class="ui-tabs-hide">' . $plugin_info->sections['changelog'] . '</div>';
 		}
 
-		$plugin_download_url = esc_url( get_plugin_info( $plugin_info['slug'], 'download_url' ) );
-		$plugin_link_url = esc_url( get_plugin_info( $plugin_info['slug'], 'link_url' ) );
-		$plugin_name = get_plugin_info( $plugin_info['slug'], 'name' );
-		$plugin_version = get_plugin_info( $plugin_info['slug'], 'version' );
-		$plugin_downloaded = get_plugin_info( $plugin_info['slug'], 'downloaded' );
-		$plugin_updated_ago = get_plugin_info( $plugin_info['slug'], 'updated_ago' );
-		$plugin_requires = get_plugin_info( $plugin_info['slug'], 'requires' );
-		$plugin_tested = get_plugin_info( $plugin_info['slug'], 'tested' );
-		$plugin_star_rating = get_plugin_info( $plugin_info['slug'], 'star_rating' );
-		$plugin_rating = get_plugin_info( $plugin_info['slug'], 'rating' );
-		$plugin_num_ratings = get_plugin_info( $plugin_info['slug'], 'num_ratings' );
 		$github_item = '';
-		$github_url = get_post_meta( get_the_ID(), 'github_url', true );
+		$github_url = get_registered_metadata( 'post', get_the_ID(), '_plugin_github_url' );
 		if ( $github_url && ! is_wp_error( $github_url ) ) {
-			$github_item = '<li><strong><a href="' . esc_url( $github_url ) . '">Contribute on Github</a></li>';
+			$github_item = '<li><strong><a href="' . esc_url( $github_url ) . '">Contribute on Github</a></strong></li>';
 		}
+		$plugin_info->plugin_link_url = esc_html( "https://wordpress.org/plugins/{$plugin_info->slug}/" );
+		$plugin_info->last_updated_ago = esc_html(  sprintf( __( '%s ago', 'my-plugins' ), human_time_diff( strtotime( $plugin_info->last_updated ) ) ) );
 
-		$plugin_info['details'] = <<<CONTENT
+		$plugin_info->details = <<<CONTENT
         <ul>
-            <li><strong>Latest Version:</strong> <a href="{$plugin_download_url}">{$plugin_version}</a></li>
+            <li><strong>Latest Version:</strong> <a href="{$plugin_info->download_link}">{$plugin_info->version}</a></li>
             {$github_item}
-            <li><strong><a href="{$plugin_link_url}">View on WordPress.org</a></li>
-            <li><strong>Downloads:</strong> {$plugin_downloaded}</li>
-            <li><strong>Last Updated:</strong> {$plugin_updated_ago}</li>
-            <li><strong>Requires WordPress Version:</strong> {$plugin_requires}+</li>
-            <li><strong>Tested compatible up to:</strong> {$plugin_tested}</li>
+            <li><strong><a href="{$plugin_info->plugin_link_url}">View on WordPress.org</a></strong></li>
+            <li><strong>Active Installs:</strong> {$plugin_info->active_installs}</li>
+            <li><strong>Last Updated:</strong> {$plugin_info->last_updated_ago}</li>
+            <li><strong>Requires WordPress Version:</strong> {$plugin_info->requires}+</li>
+            <li><strong>Tested compatible up to:</strong> {$plugin_info->tested}</li>
+            <li><strong>Rating:</strong> {$this->_get_stars( $plugin_info->rating )}</li>
         </ul>
 CONTENT;
 
 		$details_tab = '<li><a href="#tabs-details">Details</a></li>';
-		$details_tab_content = '<div id="tabs-details" class="ui-tabs-hide">' . $plugin_info['details'] . '</div>';
+		$details_tab_content = '<div id="tabs-details" class="ui-tabs-hide">' . $plugin_info->details . '</div>';
 
 		$content = <<<CONTENT
 	<div class="entry-content">
-		<a href="{$plugin_download_url}" class="big button">Download</a>
+		<a href="{$plugin_info->download_link}" class="big button">Download</a>
 		<div class="tabbed-content">
 			<div class="tabs-wrapper">
 				<ul class="tabs">
@@ -453,7 +407,7 @@ CONTENT;
 				</ul>
 			</div>
 		<div id="tabs-description">
-			{$plugin_info['description']}
+			{$plugin_info->sections['description']}
 		</div>
 		{$installation_tab_content}
 		{$faq_tab_content}
